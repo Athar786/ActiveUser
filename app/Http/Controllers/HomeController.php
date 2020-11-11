4f37f50f9d37ffa5313dd6bafbae278e01dbb0f1
athar-addweb <athar.addweb@gmail.com>
2020-11-11 09:55:48 +0000

    Active Middleware user

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+use Illuminate\Support\Facades\Auth;
+use App\Support\Collection;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        $users = Auth::user();
+        return view('home',compact('users'));
+    }
+
+   
+}
